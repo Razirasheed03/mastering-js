1fb00eac2db146d7f793d40346a543288a8a51cd
Razirasheed03 <razirasheed03@gmail.com>
2025-04-16 22:25:28 +0530

    shadowing

--- hoisting.js
+++ hoisting.js
@@ -4,3 +4,16 @@ var x='hii its from x and print undefined'
 
 console.log(y)
 let y='reference error cannot access'
+
+
+
+////////==========shadowing==========/////////
+
+var a = 1; 
+
+function testScope() {
+  console.log(a);
+  var a = 2; //////show undefined because hoisting performs and shadow the outer var
+}
+
+testScope();
