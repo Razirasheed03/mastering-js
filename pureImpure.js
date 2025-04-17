7e6a5b8ee87e143bc9b22a982ef1e24f264e2875
Razirasheed03 <razirasheed03@gmail.com>
2025-04-17 22:55:44 +0530

    pure and impure functions

---
+++ pureImpure.js
@@ -0,0 +1,15 @@
+///pure function
+
+function add(a,b){
+    return a+b
+}
+
+///impure function has sideeffects
+
+let x=15;
+function add(a){
+    x=x+a
+    return x
+}
+
+// console.log(add(10))
