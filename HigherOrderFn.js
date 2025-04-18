4fa565da131032b8abc849729a180035401c0d9b
Razirasheed03 <razirasheed03@gmail.com>
2025-04-18 11:00:20 +0530

    higher order function

---
+++ HigherOrderFn.js
@@ -0,0 +1,8 @@
+function greetUser(name){
+    return `hello from greetuser ${name}`
+}
+function hocfn(name,callbfn){
+    return callbfn(name)
+}
+console.log(greetUser('first one'))
+console.log(hocfn('second', greetUser))
