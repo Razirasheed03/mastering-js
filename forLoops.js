50f7f3c6bded7c481eab6102764805c6f096bb1d
Razirasheed03 <razirasheed03@gmail.com>
2025-04-18 15:47:58 +0530

    for in and for of loops

---
+++ forLoops.js
@@ -0,0 +1,15 @@
+///for of 
+
+const names=['razi','nadeem','akhil']
+for(let val of names){
+    // console.log(val)
+}
+
+///for in 
+const obj={
+    name:'razi',
+    age:18
+}
+for(let key in obj){
+    console.log(key,':',obj[key])
+}
