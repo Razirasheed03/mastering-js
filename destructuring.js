f2334b7694bc21e395795a524634445e06290681
Razirasheed03 <razirasheed03@gmail.com>
2025-04-19 14:41:32 +0530

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+///////array destruturing
+
+let arr=[1,2,3]
+const [a,b,c]=arr
+console.log(a,b,c)
+
+/////object destructuring
+
+let obj={name:'razi',age:19}
+const {name,age}=obj
+console.log(name)
