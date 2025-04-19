656d13e0211ab244bc820e5b67fac407388338df
Razirasheed03 <razirasheed03@gmail.com>
2025-04-19 15:02:51 +0530

    flat,foreach methods

---
+++ flat.js
@@ -0,0 +1,7 @@
+////flat array based on () value
+
+let arr=[1,2,3,[4,5,[6,7]]]
+
+console.log(arr.flat())
+console.log(arr.flat(2))
+
---
+++ foreach.js
@@ -0,0 +1,6 @@
+///foreach
+
+let arr=[1,2,3,4,5]
+arr.forEach((num,index)=>{
+    console.log(num,'and',index)
+})
