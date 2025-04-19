14e194b15cfa8a118420c3e9c814f1326795b9a8
Razirasheed03 <razirasheed03@gmail.com>
2025-04-19 22:33:22 +0530

    slice array method

--- sliceSplice.js
+++ sliceSplice.js
@@ -1,11 +1,11 @@
 ////splice
 
-let arr=[1,2,3,4,5]
-arr.splice(1,2)
+let arr = [1, 2, 3, 4, 5]
+arr.splice(1, 2)
 console.log(arr)
 
 
 ///slice
-
-let arr1=arr.slice(1,4)
+let arrays = [1, 2, 3, 4, 5]
+let arr1 = arrays.slice(1, 5)
 console.log(arr1)
