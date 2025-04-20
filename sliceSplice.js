dcc766289e1b1791546ba7f7e6d384245667a6b5
Razirasheed03 <razirasheed03@gmail.com>
2025-04-20 20:30:47 +0530

    small changes

--- sliceSplice.js
+++ sliceSplice.js
@@ -2,10 +2,12 @@
 
 let arr = [1, 2, 3, 4, 5]
 arr.splice(1, 2)
-console.log(arr)
+// console.log(arr)
 
 
 ///slice
 let arrays = [1, 2, 3, 4, 5]
 let arr1 = arrays.slice(1, 5)
-console.log(arr1)
+// console.log(arr1)
+
+
