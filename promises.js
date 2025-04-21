44eca3c63ff2d7746f5e9990e2bdf502590f98d5
Razirasheed03 <razirasheed03@gmail.com>
2025-04-21 09:59:57 +0530

    promise all

--- promises.js
+++ promises.js
@@ -8,4 +8,13 @@ let myPromise=new Promise((resolve,reject)=>{
         reject('its false')
     }
 })
-myPromise.then((res)=>console.log(res))
+// myPromise.then((res)=>console.log(res))
+
+////promise.all
+let promise1=new Promise((resolve)=>setTimeout(resolve,2000,'first'))
+let promise2=new Promise((resolve)=>setTimeout(resolve,3000,'second'))
+
+promise1.then((res)=>console.log(res))
+// Promise.all([promise1, promise2])
+//   .then((results) => console.log(results)) // ["First", "Second"]
+//   .catch((error) => console.log(error));
