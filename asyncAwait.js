4784d6f94ade6637bf4de87c47667af322d56333
Razirasheed03 <razirasheed03@gmail.com>
2025-04-24 12:08:44 +0530

    asynchronous data fetching

---
+++ asyncAwait.js
@@ -0,0 +1,7 @@
+async function fetchData() {
+    let res=await fetch("http://numbersapi.com/1")
+    let data= await res.json()
+    console.log(data,'rec')
+}
+
+///////async always return a promise
