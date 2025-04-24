8b58060df38466f9d21c50a14e75fba32e40f18b
Razirasheed03 <razirasheed03@gmail.com>
2025-04-24 11:49:44 +0530

    promise all settled methods added

--- promises.js
+++ promises.js
@@ -10,21 +10,22 @@ let myPromise = new Promise((resolve, reject) => {
 })
 // myPromise.then((res)=>console.log(res))
 
-let p1=new Promise((resolve)=>{
-    setTimeout(()=>{
+let p1 = new Promise((resolve) => {
+    setTimeout(() => {
         resolve('first')
-    },1000)
+    }, 1000)
 })
-let p2=new Promise((resolve)=>{
-    setTimeout(()=>{
+let p2 = new Promise((resolve) => {
+    setTimeout(() => {
         resolve('second')
-    },2000)
+    }, 2000)
 })
 
 // Promise.all([p1,p2]).then((results)=>console.log(results)) ////////////both should be resolve
 
 // Promise.race([p1,p2]).then((results)=>console.log(results)) /////////first one wins
 
-Promise.any([p1,p2]).then((result)=>console.log(result))      /////first one wins if reject also
+// Promise.any([p1,p2]).then((result)=>console.log(result))      /////first one wins if reject also
 
+Promise.allSettled([p1, p2]).then((result) => console.log(result))    ///////show status of each
 
