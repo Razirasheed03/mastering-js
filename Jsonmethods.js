a49e894f54a1828d7a6ef1fdf46983cf75d6af98
Razirasheed03 <razirasheed03@gmail.com>
2025-04-24 12:24:17 +0530

    json methods

---
+++ Jsonmethods.js
@@ -0,0 +1,13 @@
+///////converts javascript object to json string
+
+const user = { name: "Razi", age: 25 };
+const jsonStr = JSON.stringify(user);
+console.log(jsonStr); // '{"name":"Razi","age":25}'
+
+
+//////////converts json to  javascript object 
+const jsonStr1 = '{"name":"Razi","age":25}';
+const user1 = JSON.parse(jsonStr);
+console.log(user.name); // Razi
+
+
