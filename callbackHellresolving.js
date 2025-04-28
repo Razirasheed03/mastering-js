c94d4754224d0a7ef483f4ccfa15a5215fc55dbd
Razirasheed03 <razirasheed03@gmail.com>
2025-04-28 11:36:16 +0530

    callback hell resolved using promise

--- callbackHellresolving.js
+++ callbackHellresolving.js
@@ -10,10 +10,33 @@ function second(callback){
         callback()
     },2000)
 }
-
-
 first(()=>{
     second(()=>{
         console.log('task completed')
     })
 })
+
+/////////call back hell formed ⬆️ 
+
+//////////resolving it through promise⬇️
+
+function first(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            console.log('first completed')
+            resolve()
+        },1000)
+    })
+}
+function second(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            console.log('second completed')
+            resolve()
+        },1000)
+    })
+}
+
+first()
+.then(()=>second())
+.then(()=>console.log('task completed'))
