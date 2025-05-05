15d29adaa39a499f31f4a04c0d4403e774eb9112
Razirasheed03 <razirasheed03@gmail.com>
2025-05-06 00:24:57 +0530

    small changes

--- ObjectProbs/prob.js
+++ ObjectProbs/prob.js
@@ -1,24 +1,24 @@
-const sumOfNestedObject=(obj)=>{
-    let sum=0
-        for(let key in obj){
-            if(typeof obj[key]==='number'){
-                sum+=obj[key]
-            }else if(typeof obj[key]==='object'){
-                sum+=sumOfNestedObject(obj[key])
-            }
-        }
-        return sum
-    }
-    
-    let obj={
-        a:10,
-        b:20,
-        c:{
-            c1:10,
-            c2:20,
-            c3:{
-                c4:10
-            }
+const sumOfNestedObject = (obj) => {
+    let sum = 0
+    for (let key in obj) {
+        if (typeof obj[key] === 'number') {
+            sum += obj[key]
+        } else if (typeof obj[key] === 'object') {
+            sum += sumOfNestedObject(obj[key])
         }
     }
-    console.log(sumOfNestedObject(obj))
+    return sum
+}
+
+let obj = {
+    a: 10,
+    b: 20,
+    c: {
+        c1: 10,
+        c2: 20,
+        c3: {
+            c4: 10
+        }
+    }
+}
+console.log(sumOfNestedObject(obj))
