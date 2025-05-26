07861eaf319d4a5278c1a1e495f5cf9ab4556a1e
Razirasheed03 <razirasheed03@gmail.com>
2025-05-26 17:18:11 +0530

    page restructured

--- Practice.js
+++ Practice.js
@@ -1,7 +1,7 @@
-function palidromeChecker(str){
-    let len=str.length-1
-    for(let i=0;i<len;i++){
-        if(str[i]!==str[len-i]){
+function palidromeChecker(str) {
+    let len = str.length - 1
+    for (let i = 0; i < len; i++) {
+        if (str[i] !== str[len - i]) {
             return false
         }
     }
@@ -10,27 +10,27 @@ function palidromeChecker(str){
 }
 // console.log(palidromeChecker('malayalam'))
 
-function reverseStr(str){
-    let reversed=''
-    for(let i=str.length-1;i>=0;i--){
-         reversed+=str[i]
+function reverseStr(str) {
+    let reversed = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]
     }
     return reversed
 }
 // console.log(reverseStr('hello world'))
 
-function reverseStr(str){
-    let words=[]
-    let word=" "
-    for(let i=0;i<=str.length;i++){
-        if(str[i]===" "||i===str.length){
+function reverseStr(str) {
+    let words = []
+    let word = " "
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] === " " || i === str.length) {
             words.push(word)
-            word=""
-        }else{
-            word+=str[i]
+            word = ""
+        } else {
+            word += str[i]
         }
     }
-    for(let j=words.length-1;j>=0;j--){
+    for (let j = words.length - 1; j >= 0; j--) {
         console.log(words[j])
     }
 }
--- flat.js
+++ flat.js
@@ -1,6 +1,6 @@
 ////flat array based on () value
 
-let arr=[1,2,3,[4,5,[6,7]]]
+let arr = [1, 2, 3, [4, 5, [6, 7]]]
 
 console.log(arr.flat())
 console.log(arr.flat(2))
