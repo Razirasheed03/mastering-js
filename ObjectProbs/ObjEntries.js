6c42e3e9a95246592fb830a22e3e8e1fd279245b
Razirasheed03 <razirasheed03@gmail.com>
2025-06-04 21:24:53 +0530

    structured pages

--- ObjectProbs/ObjEntries.js
+++ ObjectProbs/ObjEntries.js
@@ -1,13 +1,13 @@
-let obj= {
-    a:5,
-    b:7,
-    d:1
+let obj = {
+    a: 5,
+    b: 7,
+    d: 1
 }
 let max = 0;
-for(let [key,value] of Object.entries(obj)){
+for (let [key, value] of Object.entries(obj)) {
     console.log()
-    max = Math.max(max,value)
-    
+    max = Math.max(max, value)
+
 }
 console.log(max)
 delete obj['b']
