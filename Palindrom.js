9776b2199ff3daa861509b0cbb8c1f0e3b7753a4
Razirasheed03 <razirasheed03@gmail.com>
2025-07-19 10:30:44 +0530

    added Palidrome using Recursion

--- Palindrom.js
+++ Palindrom.js
@@ -12,3 +12,12 @@ function isPalindrome(str) {
 
 console.log(isPalindrome("madam"));
 console.log(isPalindrome("razi")); 
+
+
+function PalindromeUsingRec(str){
+    if(str.length<=1) return true
+    if(str[0]!==str[str.length-1]) return false
+    return PalindromeUsingRec(str.slice(1,-1))
+}
+console.log(PalindromeUsingRec("madam"))
+console.log(PalindromeUsingRec("razi"))
