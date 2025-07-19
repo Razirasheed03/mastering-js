89ed272af12ce08a199f56fdc0814daca3e21cb4
Razirasheed03 <razirasheed03@gmail.com>
2025-07-19 10:31:04 +0530

    structured page

--- Palindrom.js
+++ Palindrom.js
@@ -1,23 +1,23 @@
 function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
+    let left = 0;
+    let right = str.length - 1;
 
-  while (left < right) {
-    if (str[left] !== str[right]) return false;
-    left++;
-    right--;
-  }
-  return true;
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 }
 
 console.log(isPalindrome("madam"));
-console.log(isPalindrome("razi")); 
+console.log(isPalindrome("razi"));
 
 
-function PalindromeUsingRec(str){
-    if(str.length<=1) return true
-    if(str[0]!==str[str.length-1]) return false
-    return PalindromeUsingRec(str.slice(1,-1))
+function PalindromeUsingRec(str) {
+    if (str.length <= 1) return true
+    if (str[0] !== str[str.length - 1]) return false
+    return PalindromeUsingRec(str.slice(1, -1))
 }
 console.log(PalindromeUsingRec("madam"))
 console.log(PalindromeUsingRec("razi"))
